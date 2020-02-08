529fbd7e3723d537b78959325b21699fe9ef41e5
Dani Akash <DaniAkash@users.noreply.github.com>
2020-02-08 09:14:42 +0000

    created admin user with virtuals for password

---
+++ src/models/admin.js
@@ -0,0 +1,37 @@
+require('../config/config');
+const mongoose = require("mongoose");
+const { generateHash } = require("../utils/hash");
+const { Schema, model } = mongoose;
+
+const AdminSchema = new Schema({
+  email:{
+    type: String,
+    required: true,
+  },
+  passwordHash: {
+    type: String
+  }
+});
+
+AdminSchema.virtual('password')
+  .set(function(passwordPlainText) {
+    generateHash(passwordPlainText)
+      .then(hash => {
+        this.set({ passwordHash: hash });
+      })
+      .catch(console.error)
+  });
+
+const Admin = model("Admin", AdminSchema);
+
+const adminUser = new Admin({
+  email: "admin@example.com",
+  password: "123456!"
+});
+
+adminUser.save().then((response) => {
+  console.log(response);
+}).catch(console.error);
+
+
+module.exports = Admin;
